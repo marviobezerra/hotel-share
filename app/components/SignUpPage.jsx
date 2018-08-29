07b107c0191332b32024a0b04afb703cc42cf1cd
Luciana Gomes <lugomes.eq@gmail.com>
2018-08-29 09:15:48 -0800

    signup page

--- app/components/SignUpPage.jsx
+++ app/components/SignUpPage.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import TextField from '@material-ui/core/TextField';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import { AccountCircle, Email, Phone, Lock, Cake, CropOriginal} from '@material-ui/icons/';
+import { Button, Radio, RadioGroup, FormControl, FormControlLabel, Avatar } from '@material-ui/core/';
 
 export default class SignUpPage extends React.Component {
   constructor(props) {
@@ -17,9 +21,11 @@ export default class SignUpPage extends React.Component {
   }
 
   signup(e) {
+    console.log(this.state);
     e.preventDefault();
     axios.post('/api/register', this.state)
     .then(resp => {
+      console.log(resp.data.success);
       if(resp.data.success) this.props.app.setState({auth: true});
     });
   }
@@ -27,37 +33,105 @@ export default class SignUpPage extends React.Component {
   render() {
     return (
       <div className="login-box">
-        <div className="center-vertically">
-          <img className="login-icon"
-            src="https://images.vexels.com/media/users/3/130187/isolated/lists/5e8d2205ecc8cde3235581fc5ecfa430-email-outline-icon.png" />
-          <input type="email" className="login-input" placeholder="Email"
-            onChange={(e) => this.setState({username: e.target.value})}/>
-        </div>
-        <div className="center-vertically">
-          <img className="login-icon"
-            src="https://images.vexels.com/media/users/3/132074/isolated/preview/0117cb0129593faa02646a8277ca80e3-security-lock-icon-by-vexels.png" />
-          <input type="text" className="login-input" placeholder="First Name"
-            onChange={(e) => this.setState({fname: e.target.value})}/>
-        </div>
-        <div className="center-vertically">
-          <img className="login-icon"
-            src="https://images.vexels.com/media/users/3/132074/isolated/preview/0117cb0129593faa02646a8277ca80e3-security-lock-icon-by-vexels.png" />
-          <input type="text" className="login-input" placeholder="Last Name"
+        {/* <input type="text" className="login-input" placeholder="First Name"
+          onChange={(e) => this.setState({fname: e.target.value})}/>
+        <input type="text" className="login-input" placeholder="Last Name"
             onChange={(e) => this.setState({lname: e.target.value})}/>
-        </div>
-        <div className="center-vertically">
-          <img className="login-icon"
-            src="https://images.vexels.com/media/users/3/132074/isolated/preview/0117cb0129593faa02646a8277ca80e3-security-lock-icon-by-vexels.png" />
-          <input type="password" className="login-input" placeholder="Password"
-            onChange={(e) => this.setState({password: e.target.value})}/>
-        </div>
-        <div>
-          <input type="date" placeholder="Birthdate" onChange={(e) => this.setState({birthday: e.target.value})}/>
-          <input type="number" placeholder="Phone" onChange={(e) => this.setState({phone: e.target.value})}/>
-          <input type="text" placeholder="Gender" onChange={(e) => this.setState({gender: e.target.value})}/>
-        </div>
+        <input type="email" className="login-input" placeholder="Email"
+          onChange={(e) => this.setState({username: e.target.value})}/>
+        <input type="password" className="login-input" placeholder="Password"
+          onChange={(e) => this.setState({password: e.target.value})}/>
+        <input type="date" placeholder="Birthday" onChange={(e) => this.setState({birthday: e.target.value})}/>
+        <input type="number" placeholder="Phone" onChange={(e) => this.setState({phone: e.target.value})}/>
+        <input type="radio" name="gender" value="Female" onClick={(e) => this.setState({gender: e.target.value})}/><label>Female</label>
+        <input type="radio" name="gender" value="Male" onClick={(e) => this.setState({gender: e.target.value})}/><label>Male</label>
         <button className="login-btn" onClick={(e) => this.signup(e)}>Sign Up</button>
-        <Link to="/login">Already have an account? Login</Link>
+        <Link to="/login">Already have an account? Login</Link> */}
+        {this.state.fname && this.state.lname ? <Avatar style={{backgroundColor: "orange"}}>{this.state.fname[0] + this.state.lname[0]}</Avatar> : null}
+        <TextField
+          label="First name"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <AccountCircle />
+              </InputAdornment>
+            ),
+          }}
+          onChange={(e) => this.setState({fname: e.target.value})}
+        />
+        <TextField
+          label="Last name"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <AccountCircle />
+              </InputAdornment>
+            ),
+          }}
+          onChange={(e) => this.setState({lname: e.target.value})}
+        />
+        <TextField
+          label="Email"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <Email />
+              </InputAdornment>
+            ),
+          }}
+          onChange={(e) => this.setState({username: e.target.value})}
+        />
+        <TextField
+          label="Password"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <Lock />
+              </InputAdornment>
+            ),
+          }}
+          type="password"
+          onChange={(e) => this.setState({password: e.target.value})}
+        />
+        <TextField
+          label="Phone"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <Phone />
+              </InputAdornment>
+            ),
+          }}
+          onChange={(e) => this.setState({phone: e.target.value})}
+        />
+        <TextField
+          label="Birthday"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <Cake />
+              </InputAdornment>
+            ),
+          }}
+          // InputLabelProps={{
+          //   shrink: true
+          // }}
+          type="date"
+          onChange={(e) => this.setState({birthday: e.target.value})}
+          style={{width: "95%"}}
+        />
+        <FormControl component="fieldset">
+          <RadioGroup
+            aria-label="Gender"
+            value={this.state.gender}
+            onChange={(e) => this.setState({gender: e.target.value})}
+            style={{display: "flex", flexDirection: "row"}}
+          >
+            <FormControlLabel value="Female" control={<Radio color="default"/>} label="Female" />
+            <FormControlLabel value="Male" control={<Radio color="default"/>} label="Male" />
+          </RadioGroup>
+        </FormControl>
+        <Button variant="contained" onClick={(e) => this.signup(e)} style={{margin: 20, backgroundColor: "orange", color: "white"}}>Sign Up</Button>
       </div>
     );
   }
