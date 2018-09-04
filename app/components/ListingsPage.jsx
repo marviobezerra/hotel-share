f675c000bbb223113f427112bed119a7273452d7
Diego Ibarra <dibarra@uchicago.edu>
2018-09-03 21:09:21 -0700

    Implemented interactive modal that displays listing user information upon clicking on listing

--- app/components/ListingsPage.jsx
+++ app/components/ListingsPage.jsx
@@ -220,7 +220,7 @@ class SimpleMap extends React.Component {
       </div>
       <div style={{ height: '100vh', width: '100%'}}>
         <GoogleMapReact
-          bootstrapURLKeys={{ key: 'AIzaSyBEnTOO3y2ArEsQiWsZBw1m9jbNNR2vCqw'}}
+          bootstrapURLKeys={{ key: 'Google Key goes here'}}
           defaultCenter={this.state.center}
           defaultZoom={this.state.zoom}
         >
